81b090051f08c529fee9b71b00d62ac21f7b3310
Amsterdamsik <janeair-20@mail.ru>
2015-11-19 11:26:26 +0300

    корзина

---
+++ корзина/cart.php
@@ -0,0 +1,36 @@
+<?PHP
+
+header('Content-Type: text/html; charset=utf-8');
+
+$tovar = ['name'=>'Товар','desc'=>'Описание'];
+$tovar2 = ['name2'=>'Товар2','desc2'=>'Описание2'];
+
+session_start('1');
+$_SESSION['sub'] = $_GET['sub'];
+$_SESSION['sub2'] = $_GET['sub2'];
+if(isset($_SESSION['sub'] )) {
+$_SESSION['name'] = $tovar['name'];
+$_SESSION['desc'] = $tovar['desc'];
+
+}
+if(isset($_SESSION['sub2'] )) {
+$_SESSION['name2'] = $tovar2['name2'];
+$_SESSION['desc2'] = $tovar2['desc2'];
+
+}
+?>
+
+<div> <a href="mail.php">корзина</a></div>
+<form  method="get">
+<div style="border:1px solid #000; width:100px;margin: 10px 0px 5px 0px;">
+	<h2><?php echo $tovar['name']?></h2>
+	<p><?php echo $tovar['desc']?></p>
+</div>
+	<input  type="submit" name="sub" value="+ в  корзину">
+<div style="border:1px solid #000; width:100px;margin: 10px 0px 5px 0px;">
+	<h2><?php echo $tovar2['name2']?></h2>
+	<p><?php echo $tovar2['desc2']?></p>
+</div>
+	<input  type="submit" name="sub2" value="+ в  корзину">	
+
+</form>
---
+++ корзина/mail.php
@@ -0,0 +1,62 @@
+﻿<?php
+
+
+session_start('1');
+header('Content-Type: text/html; charset=utf-8');
+$html .= '
+	<h2>'.$_SESSION['name'].'</h2>
+	<p>'.$_SESSION['desc'].'</p>';
+$html2 .= '
+	<h2>'.$_SESSION['name2'].'</h2>
+	<p>'.$_SESSION['desc2'].'</p>';
+	
+$_SESSION['mail'] = $_POST['mail'];	
+	if(isset($_SESSION['mail'])) {
+		$html_mail .= '<input type="email" name="email" placeholder="введите свой email"> ';	
+	}
+	$_SESSION['email'] = $_POST['email'];
+	
+	if(isset($_SESSION['mail']) and ($_SESSION['email'])) {//почта
+		
+		$to  = "<janeair-20@mail.ru>, " ; 
+		$to .= "<donkovtsev@icloud.com>"; 
+
+		$subject = "Ваш заказ"; 
+
+		$message = ' 
+			<html> 
+				<head> 
+					<title>Заказ товара</title> 
+				</head> 
+				<body> 
+					<h1>Спасибо за заказ!</h1>
+					<p>Наименование:'.$html.'<hr>'.$html2.'</p> 
+				
+				</body> 
+			</html>'; 
+
+		$headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+		$headers .= "From: internet-shop <me@example.com>\r\n"; 
+		$headers .= "Bcc: donkovtsev@icloud.com\r\n"; 
+
+		$send = mail($to, $subject, $message, $headers); 
+		if ($send == 'true')
+		{echo 'заказ отправлен';}
+		else {echo "Ошибка, сообщение не отправлено!";}
+
+	}
+
+?>
+<form action="" method="post">
+<div style="border:1px solid #000; width:100px;margin: 10px 0px 5px 0px;">
+
+<?php echo $html ?>
+
+
+<?php echo $html2 ?>
+</div>
+<div>
+<?php echo $html_mail ?>
+</div>
+<input  type="submit" name="mail" value="заказать">	
+</form>
