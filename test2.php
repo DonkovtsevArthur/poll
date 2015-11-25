0c9cc9cbe755d094876d2de626d9910a7b7df450
Amsterdamsik <janeair-20@mail.ru>
2015-11-25 13:11:30 +0300

    поиск  файла в папке

---
+++ test2.php
@@ -0,0 +1,29 @@
+﻿<?php
+$filename = "iphone4.png";
+$foldername = "./img";
+
+
+//способ 1
+		$files = scandir($foldername);
+		print_r($files);
+		echo "<br><br>";
+			if (in_array($filename, $files)) {
+				echo "способ №1: $foldername/$filename <br>";	
+			}
+			else {
+				echo 'no file';
+				}
+			echo "<br><br><br>";
+			
+//способ 2			
+		$handle = opendir("./img");
+		while(($item = readdir($handle)) !== false)  {
+				 if ($item != "." && $item != "..") { 
+					if($item == $filename) {
+						echo "способ №2: $foldername/$filename <br>";
+					}
+			}	
+		}
+
+?>
+
